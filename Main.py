3d9a7f1a0a380eaa8008f26bf1495abba7c6bb87
NoseyLoner <21Abademosi@parmiters.herts.sch.uk>
2025-02-27 22:32:49 +0000

    Finish initial draft logic;add input handiling to wrap up,then move on to optimising and refining

--- Main.py
+++ Main.py
@@ -32,7 +32,8 @@ class Unit:
     def Strongest(cls):
          Damages = {}
          for Enemy in cls.HostileUnits:
-             Damages[Enemy.Damage] = Enemy
+             if Enemy.Alive:
+                Damages[Enemy.Damage] = Enemy
          Strongest = max(Damages)
          return Damages[Strongest]
 
@@ -57,6 +58,9 @@ class Unit:
                 Healths[Unit.Health] = Unit.MaxHealth
         return (HealthTotal,MaxTotal)
     
+    # Add a check for the team constants,and then check units on that team
+    # Also add a check for individual units
+    # remember to add a check for "All" constant
     @classmethod
     def Check(cls):
         for Instance in cls.Units:
@@ -67,8 +71,16 @@ class Unit:
                     cls.HostileUnits.remove(Instance)
                     cls.PassiveUnits.remove(Instance)
                 except ValueError:
-                    pass    
+                    pass
+        if len(cls.HostileUnits) == 0:
+            print("Friendly Units Win!")
+            time.sleep(1)
+            exit()
+        elif len(cls.FriendlyUnits) == 0:
+            print("Hostile Units Win!")
+            time.sleep(1)    
 
+    #This might be redundant, remove if no use is found
     @classmethod
     def Split(cls):
         for Instance in cls.Units:
@@ -92,23 +104,26 @@ class Unit:
                 cls.FriendlyUnits.append(Unit(randint(4,7),randint(14,20),Constants.Passive))
 
     @classmethod
-    def Display(cls,Team):
-        if Team == Constants.Hostile:
-            print("Hostile Units")
+    def Display(cls):
+        print("-" * 28)
+        if len(cls.HostileUnits) != 0:
+            print(f"Hostile Units: {len(cls.HostileUnits)}/3")
             for i in range(len(cls.HostileUnits)):
                 if cls.HostileUnits[i].Alive:
-                    print(f"Unit{i + 1}:{cls.HostileUnits[i].Health}/{cls.HostileUnits[i].MaxHealth} Health,{cls.HostileUnits[i].Damage} Damage")
-        elif Team == Constants.Friendly:
-            print("Friendly Units")
+                    print(f"Unit {i + 1}:{cls.HostileUnits[i].Health}/{cls.HostileUnits[i].MaxHealth} Health,{cls.HostileUnits[i].Damage} Damage")
+        time.sleep(1)
+        if len(cls.FriendlyUnits) != 0:
+            print(f"\nFriendly Units: {len(cls.FriendlyUnits)}/3")
             for i in range(len(cls.FriendlyUnits)):
                 if cls.FriendlyUnits[i].Alive:
-                    print(f"Unit{i + 1}:{cls.FriendlyUnits[i].Health}/{cls.FriendlyUnits[i].MaxHealth} Health,{cls.FriendlyUnits[i].Damage} Damage")
-        else:
-            print("Passive Units")
+                    print(f"Unit {i + 1}:{cls.FriendlyUnits[i].Health}/{cls.FriendlyUnits[i].MaxHealth} Health,{cls.FriendlyUnits[i].Damage} Damage")
+        if len(cls.PassiveUnits) != 0:
+            print(f"\nPassive Units: {len(cls.PassiveUnits)}/3")
+            time.sleep(1)
             for i in range(len(cls.PassiveUnits)):
                 if cls.PassiveUnits[i].Alive:
-                    print(f"Unit{i + 1}:{cls.PassiveUnits[i].Health}/{cls.PassiveUnits[i].MaxHealth} Health,{cls.PassiveUnits[i].Damage} Damage")
-        print()
+                    print(f"Unit {i + 1}:{cls.PassiveUnits[i].Health}/{cls.PassiveUnits[i].MaxHealth} Health,{cls.PassiveUnits[i].Damage} Damage")
+        print("-" * 28,"\n")
 
 class Controller:
     
@@ -116,7 +131,6 @@ class Controller:
         self.Team = Team
 
 # Might merge with future enemy class
-# Might also generalise using *args
 def Chance(Probability:int):
     Result = randint(1,100)
     if Result <= Probability:
@@ -127,7 +141,9 @@ def Starter():
     Coin = randint(0,1)
     Guess = int(input("Guess 0 or 1: "))
     if Guess == Coin:
+        print("You guessed correctly! You start.\n")
         return True
+    print("You guessed incorrectly! Enemy starts.\n")
     return False
 
     
@@ -145,37 +161,64 @@ def Main():
         time.sleep(1)
     
     print("Ready!")
-    os.system("clear")
+    time.sleep(1)
+    os.system("cls")
 
+    print("Welcome to QTBS!")
+    time.sleep(1)
     Turn = Starter()
+    Clearer = 0
+    time.sleep(1)
     Sides = {"E":Constants.Hostile,"F":Constants.Friendly}
 
     while True:
-        Unit.Display(Constants.Hostile)
-        Unit.Display(Constants.Friendly)
+        Unit.Check()
+        Unit.Display()
+        time.sleep(1)
         if Turn:
-            print("Player's Turn")
+            print("Player's Turn:")
             Side = input("Enter the side you want to target, Enemy(E) or Friendly(F): ").capitalize()
             Side = Sides[Side]
             Target = int(input("Enter the number of the unit you want to attack: "))
             Attacker = int(input("Enter the number of the unit you want to attack with: "))
             if Side == Constants.Hostile:
                 Unit.FriendlyUnits[Attacker - 1].Attack(Unit.HostileUnits[Target - 1])
-                print(f"\nYou Attacked Unit")
+                print(f"\nYou Attacked Unit {Target} with Unit {Attacker} and dealt {Unit.FriendlyUnits[Attacker - 1].Damage} damage.\n")
+                if not Unit.HostileUnits[Target - 1].Alive:
+                    print(f"You have killed Unit {Unit.HostileUnits.index(Target) + 1}!\n")
+                time.sleep(1)
             else:
                 Unit.FriendlyUnits[Attacker - 1].Attack(Unit.FriendlyUnits[Target - 1])
+                print(f"\nYou Healed Unit {Target} with Unit {Attacker} and healed {Unit.FriendlyUnits[Attacker - 1].Damage} health.\n")
+            time.sleep(1)
         else:
+            print("Enemy's Turn:")
             Ratio = Unit.Health(Constants.Hostile)
             Ratio = round(Ratio[0]/Ratio[1],2) * 100
             Attack = Chance(Ratio)
             Strongest = Unit.Strongest()
             if Attack:
-                Strongest.Attack(choice(Unit.FriendlyUnits))
+                Target = choice(Unit.FriendlyUnits)
+                Strongest.Attack(Target)
+                time.sleep(2)
+                print(f"Enemy Attacked Unit {Unit.FriendlyUnits.index(Target) + 1} with Unit {Unit.HostileUnits.index(Strongest) + 1},dealt {Strongest.Damage} damage.\n")
+                time.sleep(1)
+                if not Target.Alive:
+                    print(f"The Enemy has killed Unit {Unit.FriendlyUnits.index(Target) + 1}!\n")
+                time.sleep(1)
             else:
                 Weakest = Unit.Weakest()
                 Strongest.Attack(Weakest)
+                time.sleep(2)
+                print(f"Enemy Healed Unit {Unit.HostileUnits.index(Strongest) + 1} with Unit {Unit.HostileUnits.index(Strongest) + 1},healed {Strongest.Damage} health.\n")
+                time.sleep(1)
         
         Turn = not Turn
         Unit.Check()
+        Clearer += 1
+        if Clearer % 2 == 0:
+            time.sleep(2)
+            os.system("cls")
+            Clearer = 0
 
 Main()
