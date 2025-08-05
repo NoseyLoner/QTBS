621cb12465ff911a732ee458664eb08fd69ba365
NoseyLoner <21Abademosi@parmiters.herts.sch.uk>
2025-08-05 21:31:42 +0000

    TODO2

--- Src/Main.py
+++ Src/Main.py
@@ -1,6 +1,11 @@
 import os
 from time import sleep
-from Observers import Director,Controller
+from Observers import Controller
+from Constants import Constants
+from Entities import Unit,GameOverException
+from random import randint,choice
+from math import floor,ceil
+from StatusEffects import StatusEffect
 
 # Game Progress:
 #   For the text based 1.0, as of now (08/03/25) mostly everything up to quantum stuff has been implemented in code, and just needs actully substance/testing:
@@ -11,22 +16,172 @@ from Observers import Director,Controller
 #   Once all of the above is done, the only thing left is to make sure the game is actully fun (as right now it is boring, tedious and repetitive) and then release
 #   Then, visuals will begin with the help of Pygame
 
-Administrator = Director()
-Player = Controller(Constants.Friendly)
-Enemy = Controller(Constants.Hostile)
-Administrator.Attach(Player)
-Administrator.Attach(Enemy)
-
 class GameOverException(Exception):
     pass
 
-def ApplyEffects(Unit:'Unit'):
-    ...
+Player = Controller(Constants.Friendly)
+Enemy = Controller(Constants.Hostile)
 
 # WARNING! Unit ID needs to be added!
 # WARNING! CanAttack needs to be used in the Attack method!
 # self.ID = len(Unit.Units[Team]) is a simple way to add it, but it's probably not the way you want to do it
 # Although it would be easy to replace the name system with
+class Unit:
+
+    Units:dict[Constants,list['Unit']] = {Constants.Friendly:[],Constants.Hostile:[],Constants.Passive:[]}
+
+    # Applies argument might be redundant?
+    # Might Change Health to HP
+    def __init__(self,Damage:int,MaxHealth:int,Armour:int,Team:Constants,Name:str,Applies:dict[Constants,list['StatusEffect']] = {Constants.Buffs:[],Constants.Nerfs:[]}):
+        Unit.Units[Team].append(self)
+        self._Alive = True
+        self.CanAttack:bool = True
+        self.OverHeal:bool = False
+        self.Damage = Damage
+        self.MaxHealth = MaxHealth
+        self._Health = MaxHealth
+        self.Armour = Armour
+        self.Team = Team
+        self.Applies = Applies
+        self.Name = Name
+        self.Affected:list['StatusEffect'] = []
+        
+    @property
+    def Alive(self):
+        return self._Alive
+
+    # Add stuff here when quantum part is added
+    @Alive.setter
+    def Alive(self,Status:bool):
+        if Status:
+            pass
+        else:
+            del self
+
+    @property
+    def Health(self):
+        return self._Health
+    
+    # This is done for now I think
+    # Floor and Ceiling need to added to deal with Targeted and Armoured
+    @Health.setter
+    def Health(self,Value:int):
+        if Value < 0:
+            self._Health -= round(Value / self.Armour)
+            if self._Health <= 0:
+                self._Health = 0
+                self.Alive = False
+        elif Value > 0:
+            self._Health += Value
+            if self._Health > self.MaxHealth and not self.OverHeal:
+                self._Health = self.MaxHealth
+
+    def Attack(self,Target:'Unit'):
+        if Target.Team is not self.Team:
+            Target.Health -= self.Damage
+            Director.Update(Target.Team,Target.Name,f"{self.Team.value} {self.Name} has attacked {Target.Team.value} {Target.Name}.")
+            # for now, status effects are sure hit, but will be chance based later on
+            if self.Applies[Constants.Nerfs]:
+                for Effect in self.Applies[Constants.Nerfs]:
+                    Effect.Apply(Target)
+                    Director.Update(Target.Team,Target.Name,f"{self.Team.value} {self.Name} has nerfed {Target.Team.value} {Target.Name} with {Effect.Name}.")
+        else:
+            # When Enchantments are added, change this to allow overhealing if the unit has a certain level of mending
+            if Target.Health >= Target.MaxHealth:
+                # Might get  rid of this message later
+                print(f"{Target.Name} Cannot be healed further.")
+            else:
+                Target.Health += self.Damage
+            # Same as above for buffs
+            if self.Applies[Constants.Buffs]:
+                for Effect in self.Applies[Constants.Buffs]:
+                    Effect.Apply(Target)
+                    Director.Update(Target.Team,Target.Name,f"{self.Team.value} {self.Name} has buffed {Target.Team.value} {Target.Name} with {Effect.Name}.")
+            
+
+    @classmethod
+    def Strongest(cls,Team:Constants) -> 'Unit':
+         Damages = {}
+         for Enemy in cls.Units[Team]:
+             if Enemy.Alive:
+                Damages[Enemy.Damage] = Enemy
+         Strongest = max(Damages)
+         return Damages[Strongest]
+
+    @classmethod
+    def Weakest(cls,Team:Constants) -> 'Unit':
+        Healths = {}
+        for Enemy in cls.Units[Team]:
+            if Enemy.Alive:
+                Healths[Enemy.Health] = Enemy
+        Weakest = min(Healths)
+        return Healths[Weakest]
+
+    # Might change if enemy AI is changed
+    # Healths dictionary was here before, might be useful
+    @classmethod
+    def Healths(cls,Team):
+        HealthTotal = 0
+        MaxTotal = 0
+        for Unit in cls.Units[Team]:
+            HealthTotal += Unit.Health
+            MaxTotal += Unit.MaxHealth
+        return (HealthTotal,MaxTotal)
+    
+    # Change name to Collapse when quantum stuff is added
+    @classmethod
+    def Check(cls,Team:Constants = Constants.All):
+        if Team == Constants.All:
+            for Team in cls.Units:
+                for Unit in cls.Units[Team]:
+                    if Unit.Health <= 0:
+                        Unit.Alive = False
+                        cls.Units[Team].remove(Unit)
+        else:
+            for Unit in cls.Units[Team]:
+                if Unit.Health <= 0:
+                    Unit.Alive = False
+                    cls.Units[Team].remove(Unit)
+        if len(cls.Units[Constants.Friendly]) == 0:
+            raise GameOverException("Friendly Units Win!")
+        elif len(cls.Units[Constants.Hostile]) == 0:
+            raise GameOverException("Hostile Units Win!")  
+
+    @classmethod
+    def Display(cls,Teams:list[Constants] = Constants.All):
+        if Teams == Constants.All:
+            for ATeam in cls.Units:
+                print("-" * 28)
+                print(f"{ATeam.value} Units: {len(cls.Units[ATeam])}/3")
+                for AUnit in cls.Units[ATeam]:
+                    print(f"{AUnit.Name}: Health: {AUnit.Health}/{AUnit.MaxHealth}, Damage: {AUnit.Damage}")
+                print("-" * 28,"\n")
+                sleep(1)
+        else:
+            for Team in Teams:
+                print("-" * 28)
+                print(f"{Team.value} Units: {len(cls.Units[Team])}/3")
+                for Unit in cls.Units[Team]:
+                    print(f"{Unit.Name}: Health: {Unit.Health}/{Unit.MaxHealth}, Damage: {Unit.Damage}")
+                print("-" * 28,"\n")
+                sleep(1)
+
+    # This is fine,i think?
+    @classmethod
+    def Create(cls,Amount:int,Team:Constants = Constants.All):
+        if Team == Constants.All:
+            for ATeam in cls.Units:
+                for i in range(Amount):
+                    cls.Units[ATeam].append(Unit(randint(4,7),randint(14,20),ATeam,f"Unit {i + 1}"))
+        else:
+            for i in range(Amount):
+                cls.Units[Team].append(Unit(randint(4,7),randint(14,20),Team,f"Unit {i + 1}"))
+                cls.Units[Team].pop()
+
+    # You might want to check is the unit already applies this effect
+    @classmethod
+    def Imbude(cls,IUnit:'Unit',Effect:'StatusEffect'):
+        IUnit.Applies[Effect.Sign].append(Effect)
 
 class Tools:
 
--- Src/Observers.py
+++ Src/Observers.py
@@ -1,16 +1,4 @@
 from Constants import Constants
-import Main
-
-# All code below should work up too status effects, not sure if more needs to be done
-# Also, to my knowledge this is the most advanced code I have not only written, but understood
-class Singleton(type):
-    
-    Instances = {}
-
-    def __call__(cls,*args,**kwargs):
-        if cls not in cls.Instances:
-            cls.Instances[cls] = super().__call__(*args,**kwargs)
-        return cls.Instances[cls]
     
 class Multiton(type):
 
@@ -20,48 +8,17 @@ class Multiton(type):
         if Key not in cls.Instances:
             cls.Instances[Key] = super().__call__(Key,*args,**kwargs)
         return cls.Instances[Key]
-    
-class Observer():
 
-    def Display(self):
-        pass
-
-    def Update(self):
-        pass
-
-class Director(metaclass = Singleton):
-    
-    def __init__(self):
-        self.Controllers:dict[Constants,'Controller'] = {}
-
-    def Attach(self,Controller:'Controller'):
-        self.Controllers[Controller.Team] = Controller
-
-    def Detach(self,Controller:'Controller'):
-        self.Controllers[Controller.Team] = None
-
-    # Might change name to Notify/Distribute
-    def Update(self,Team:Constants,Name:str,Message:str):
-            self.Controllers[Team].Update(Name,Message)
-
-# Might do more later on, but is probaly complete for handiling Status Effects
-class Controller(Observer,metaclass = Multiton):
+class Controller(metaclass = Multiton):
 
     def __init__(self,Team:Constants):
         self.Team = Team
-        self.Messages:dict[str,list[str]] = {}
+        self.Messages:list[str] = []
 
     def Display(self):
-        # Read eventually to make sure this is working as intended, or at all
-        # I'm losing my mind
-        for i in range(len(Main.Unit.Units[self.Team])):
-            print(f"\033[4mAlerts for the {self.Team.value} team:\033[0m")
-            for Message in self.Messages:
-                print(Message)
+        print(f"\033[4mAlerts for the {self.Team.value} team:\033[0m")
+        for Message in self.Messages:
+            print(Message)
 
-    # This doesn't work
-    # I think it's working now though,maybe...
-    def Update(self,Unit:str,Message:str):
-        if Unit not in self.Messages:
-            self.Messages[Unit] = []    
-        self.Messages[Unit].append(Message)
+    def Update(self,Message:str):
+        self.Messages.append(Message)
