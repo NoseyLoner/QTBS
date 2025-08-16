7bce6701a66b028c9efd7cc9de54bd3f039de987
NoseyLoner <21Abademosi@parmiters.herts.sch.uk>
2025-08-16 21:33:37 +0000

    Finally got back a playable version

--- Src/Main.py
+++ Src/Main.py
@@ -3,7 +3,7 @@ from time import sleep
 from Observers import Controller
 from Constants import Constants
 from random import randint,choice
-from math import floor,ceil
+from math import floor,ceil 
 
 # Game Progress:
 #   For the text based 1.0, as of now (08/03/25) mostly everything up to quantum stuff has been implemented in code, and just needs actully substance/testing:
@@ -28,16 +28,15 @@ Controllers = {Constants.Friendly:Player,Constants.Hostile:Enemy}
 # Although it would be easy to replace the name system with
 class Unit:
 
-    #Good choice
     Units:dict[Constants,dict[str,'Unit']] = {Constants.Friendly:{},Constants.Hostile:{},Constants.Passive:{}}
+    Counters:dict[Constants,int] = {Constants.Friendly:0,Constants.Hostile:0,Constants.Passive:0}
+    IDs:list[str] = []
 
     # Applies argument might be redundant?
-    def __init__(self,Damage:int,MaxHealth:int,Armour:int,Heal:int,Team:Constants,Applies:dict[Constants,list] = {Constants.Buffs:[],Constants.Nerfs:[]}):
-        Unit.Units[Team].append(self)
+    def __init__(self,Damage:int,MaxHealth:int,Armour:int,Heal:int,ID:str,Team:Constants,Applies:dict[Constants,list] = {Constants.Buffs:[],Constants.Nerfs:[]}):
         self._Alive = True
         self.CanAttack:bool = True
         self.OverHeal:bool = False
-        self.ID = Team.value[0] + str(Unit.Units[Team].index(self) + 1)
         self.Affected:list = []
         self.Controller = Controllers[Team] 
         self.Damage = Damage
@@ -45,6 +44,7 @@ class Unit:
         self._Health = MaxHealth
         self.Armour = Armour
         self.Heal = Heal
+        self.ID = ID
         self.Team = Team
         self.Applies = Applies
         
@@ -58,6 +58,8 @@ class Unit:
         if Status:
             pass
         else:
+            Unit.Units[self.Team].pop(self.ID)
+            Unit.IDs.remove(self.ID)
             del self
 
     @property
@@ -68,16 +70,18 @@ class Unit:
     # Floor and Ceiling need to added to deal with Targeted and Armoured
     @Health.setter
     def Health(self,Value:int):
-        if Value < 0:
-            self._Health -= round(Value / self.Armour)
+        if Value < self._Health:
+            Difference = self._Health - Value
+            self._Health -= round(Difference / self.Armour)
             if self._Health <= 0:
                 self._Health = 0
                 self.Alive = False
-        elif Value > 0:
-            self._Health += Value
+        elif Value > self._Health:
+            self._Health = Value
             if self._Health > self.MaxHealth and not self.OverHeal:
                 self._Health = self.MaxHealth
 
+    # Remove print statements when controller system is implemented
     def Attack(self,Target:'Unit'):
         if not self.CanAttack:
             raise CannotAttackException(f"{self.ID} cannot attack!")
@@ -85,6 +89,7 @@ class Unit:
             if Target.Team is not self.Team:
                 Target.Health -= self.Damage
                 self.Controller.Update(self.ID,f"Unit {self.ID} has attacked Unit {Target.ID}, dealing {self.Damage} damage.")
+                print(f"Unit {self.ID} has attacked Unit {Target.ID}, dealing {self.Damage} damage.")
                 # for now, status effects are sure hit, but will be chance based later on
                 if self.Applies[Constants.Nerfs]:
                     for Effect in self.Applies[Constants.Nerfs]:
@@ -97,27 +102,30 @@ class Unit:
                     print(f"{Target.ID} Cannot be healed further.")
                 else:
                     Target.Health += self.Heal
+                    print(f"Unit {self.ID} has healed Unit {Target.ID} for {self.Heal} health.")
                 # Same as above for buffs
                 if self.Applies[Constants.Buffs]:
                     for Effect in self.Applies[Constants.Buffs]:
                         Effect.Apply(Target)
                         self.Controller.Update(Target.ID,f"Unit {self.ID} has buffed Unit {Target.ID} with {Effect.Name}.")
+                        print(f"Unit {self.ID} has buffed Unit {Target.ID} with {Effect.Name}.")
 
     @classmethod
     def Strongest(cls,Team:Constants) -> 'Unit':
-         Damages = {}
-         for Enemy in cls.Units[Team]:
-             if Enemy.Alive:
-                Damages[Enemy.Damage] = Enemy
-         Strongest = max(Damages)
-         return Damages[Strongest]
+         CUnits:list['Unit'] = cls.Units[Team].values()
+         MaxDamage = 0
+         MaxCUnit = None
+         for CUnit in CUnits:
+            if CUnit.Damage > MaxDamage:
+                MaxDamage = CUnit.Damage 
+                MaxCUnit = CUnit
+         return MaxCUnit
 
     @classmethod
-    def Weakest(cls,Team:Constants) -> 'Unit':
+    def Weakest(cls,Team:Constants) -> 'Unit':  
         Healths = {}
-        for Enemy in cls.Units[Team]:
-            if Enemy.Alive:
-                Healths[Enemy.Health] = Enemy
+        for Enemy in cls.Units[Team].values():
+            Healths[Enemy.Health] = Enemy
         Weakest = min(Healths)
         return Healths[Weakest]
 
@@ -127,29 +135,18 @@ class Unit:
     def Healths(cls,Team):
         HealthTotal = 0
         MaxTotal = 0
-        for Unit in cls.Units[Team]:
+        for Unit in cls.Units[Team].values():
             HealthTotal += Unit.Health
             MaxTotal += Unit.MaxHealth
         return (HealthTotal,MaxTotal)
     
     # Change name to Collapse when quantum stuff is added
     @classmethod
-    def Check(cls,Team:Constants = Constants.All):
-        if Team == Constants.All:
-            for Team in cls.Units:
-                for Unit in cls.Units[Team]:
-                    if Unit.Health <= 0:
-                        Unit.Alive = False
-                        cls.Units[Team].remove(Unit)
-        else:
-            for Unit in cls.Units[Team]:
-                if Unit.Health <= 0:
-                    Unit.Alive = False
-                    cls.Units[Team].remove(Unit)
+    def Check(cls):
         if len(cls.Units[Constants.Friendly]) == 0:
-            raise GameOverException("Friendly Units Win!")
+            raise GameOverException("Hostile Units Win!")
         elif len(cls.Units[Constants.Hostile]) == 0:
-            raise GameOverException("Hostile Units Win!")  
+            raise GameOverException("Friendly Units Win!")  
 
     @classmethod
     def Display(cls,Teams:list[Constants] = Constants.All):
@@ -157,7 +154,7 @@ class Unit:
             for ATeam in cls.Units:
                 print("-" * 28)
                 print(f"{ATeam.value} Units: {len(cls.Units[ATeam])}/3")
-                for AUnit in cls.Units[ATeam]:
+                for AUnit in cls.Units[ATeam].values():
                     print(f"{AUnit.ID}: Health: {AUnit.Health}/{AUnit.MaxHealth}, Damage: {AUnit.Damage}")
                 print("-" * 28,"\n")
                 sleep(1)
@@ -165,7 +162,7 @@ class Unit:
             for Team in Teams:
                 print("-" * 28)
                 print(f"{Team.value} Units: {len(cls.Units[Team])}/3")
-                for Unit in cls.Units[Team]:
+                for Unit in cls.Units[Team].values():
                     print(f"{Unit.ID}: Health: {Unit.Health}/{Unit.MaxHealth}, Damage: {Unit.Damage}")
                 print("-" * 28,"\n")
                 sleep(1)
@@ -176,17 +173,18 @@ class Unit:
         if Team == Constants.All:
             for ATeam in cls.Units:
                 for i in range(Amount):
-                    ID = f"{ATeam.value[0]}{len(cls.Units[ATeam]) + 1}"
-                    cls.Units[ATeam][ID] = Unit(randint(4,7),randint(49,52),1,randint(0,4),ATeam)
+                    cls.Counters[ATeam] += 1
+                    ID = f"{ATeam.value[0]}{cls.Counters[ATeam]}"
+                    cls.Units[ATeam][ID] = Unit(randint(6,9),randint(36,45),1,randint(0,3),ID,ATeam)
+                    cls.IDs.append(ID)
         else:
             for i in range(Amount):
-                cls.Units[Team].append(Unit(randint(4,7),randint(49,52),1,randint(0,4),Team))
-                cls.Units[Team].pop()
+                cls.Counters[Team] += 1
+                ID = f"{Team.value[0]}{cls.Counters[Team]}"
+                cls.Units[Team][ID] = Unit(randint(6,9),randint(36,45),1,randint(0,3),ID,Team)
+                cls.IDs.append(ID)
+                cls.Units[Team]
 
-    # You might want to check is the unit already applies this effect
-    @classmethod
-    def Imbude(cls,IUnit:'Unit',Effect):
-        IUnit.Applies[Effect.Sign].append(Effect)
 
 class Tools:
 
@@ -267,6 +265,7 @@ class Tools:
         
 # The main function doesn't use the Administrator or Controllers to notify the player
 def Main():
+    # Not the first concept anymore
     print("QTBS: First Concept.")
 
     Unit.Create(3,Constants.Friendly)
@@ -276,14 +275,14 @@ def Main():
     
     Tutor = input("Press 'T' to play the tutorial or any other key to skip: ").capitalize()
     if Tutor == "T":
-        Tools.Wait(StartingMessage = "Starting Tutorial...")
+        Tools.Wait(Time = 1,StartingMessage = "Starting Tutorial...")
         Tools.Tutorial()
 
     Tools.Clear()
     Turn = Tools.Starter()
-    Tools.Clear()
+    sleep(1)
+    Tools.Wait(Time = 1,StartingMessage = "Starting Game...")
     Clearer = 0
-    Sides = {"E":Constants.Hostile,"F":Constants.Friendly}
     sleep(1)
 
     while True:
@@ -291,45 +290,37 @@ def Main():
             Unit.Check()
         except GameOverException as Winner:
             print(Winner)
+            input("Press enter to exit the game.")
             break
         Unit.Display([Constants.Friendly,Constants.Hostile])
         if Turn:
             print("Player's Turn:")
             while True:
                 try:
-                    Side = input("Enter the side you want to target, Enemy(E) or Friendly(F): ").capitalize()
-                    Side = Sides[Side]
-                    break
-                except KeyError:
-                    print("Invalid input, please try again.")
-            while True:
-                try:
-                    TargetName = int(input("Enter the number of the unit you want to target: "))
-                    if TargetName not in range(1,len(Unit.Units[Side]) + 1):
+                    TargetID = input("Enter the ID of the unit you want to attack/heal: ").upper()
+                    if TargetID not in Unit.IDs:
                         raise ValueError
                     break
                 except ValueError:
-                    print("Invalid input, please try again.")
+                    print("Invalid ID, please try again.")
             while True:
                 try:
-                    AttackerName = int(input("Enter the number of the unit you want to attack with: "))
-                    if AttackerName not in range(1,len(Unit.Units[Constants.Friendly]) + 1):
+                    AttackerID = input("Enter the ID of the unit you want to attack/heal with: ").upper()
+                    if AttackerID not in Unit.Units[Constants.Friendly]:
                         raise ValueError
                     break
                 except ValueError:
-                    print("Invalid input, please try again.")
-            # There might be a better way to do this 
-            # WARNING! CanAttack should probably be checked here!
-            Attacker = Unit.Units[Constants.Friendly][AttackerName - 1]
-            Target = Unit.Units[Side][TargetName - 1]
+                    print("Invalid ID, please try again.")
+            Attacker = Unit.Units[Constants.Friendly][AttackerID]
+            if TargetID[0] == "H":
+                Target = Unit.Units[Constants.Hostile][TargetID]
+            elif TargetID[0] == "F":
+                Target = Unit.Units[Constants.Friendly][TargetID]
+            # Unused for now
+            else:
+                Target = Unit.Units[Constants.Passive][TargetID]
             Attacker.Attack(Target)
             sleep(1)
-            if Side == Constants.Hostile:
-                print(f"\nYou Attacked {Target.ID} with Unit {Attacker.ID} and dealt {Attacker.Damage} damage.")
-                if not Target.Alive:
-                    print(f"You have killed {Target.ID}!")
-            else:
-                print(f"\nYou Healed {Target.ID} with {Attacker.ID} and healed {Attacker.Damage} health.")
         else:
             print("Enemy's Turn:")
             Ratio = Unit.Healths(Constants.Hostile)
@@ -337,10 +328,8 @@ def Main():
             Attack = Tools.Chance(Ratio)
             Strongest = Unit.Strongest(Constants.Hostile)
             if Attack:
-                Target = choice(Unit.Units[Constants.Friendly])
+                Target = choice(list(Unit.Units[Constants.Friendly].values()))
                 Strongest.Attack(Target)
-                sleep(1)
-                print(f"Enemy Attacked {Target.ID} with {Strongest.ID},dealt {Strongest.Damage} damage.")
                 sleep(1)
                 if not Target.Alive:
                     print(f"The Enemy has killed {Target.ID}!")
@@ -348,8 +337,6 @@ def Main():
             else:
                 Weakest = Unit.Weakest(Constants.Hostile)
                 Strongest.Attack(Weakest)
-                sleep(1)
-                print(f"Enemy Healed {Weakest.ID} with {Strongest.ID},healed {Strongest.Damage} health.")
                 sleep(1)
         
         Exit = input("Press 'E' to exit the game or any other key to continue: ").capitalize()
