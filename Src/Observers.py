bd29f886ea93458ff5c5e6869f8a71e137641d9e
NoseyLoner <21Abademosi@parmiters.herts.sch.uk>
2025-09-08 20:59:50 +0000

    Started to adda good display method

--- Src/Observers.py
+++ Src/Observers.py
@@ -16,74 +16,69 @@ class Multiton(type):
 # WIP!!!
 class Observer:
 
-    Messages:dict[Constants,dict[str,dict[Constants,dict[str,str]]]] = {
+    Messages:dict[Constants,dict[str,list[dict[str,str | Constants]]]] = {
         Constants.Start:{},
         Constants.End:{},
         Constants.Shopping:{}}
 
-    def Display(self,State:Constants):
+    def Display(self,State:Constants,Team:Constants):
         match State:
             case Constants.Start:
                 if len(self.Messages[Constants.Start]) > 0:
-                    Width = get_terminal_size().columns()
-                    rich.print(f"[underline]Start Of Turn Updates:")
-                    FTitle = f"[underline]Friendly:[/underline]"
-                    HTitle = f"[underline]Hostile:[/underline]"
-                    print(f"{FTitle:<{Width//2}}{HTitle}")
-                    FSIDs = [FIDs for FIDs in self.Messages[Constants.Start].keys() if "F" in FIDs]
-                    HSIDs = [HIDs for HIDs in self.Messages[Constants.Start].keys() if "H" in HIDs]
-                    if len(FSIDs) > 0 and len(HSIDs) > 0:
-                        FSUpdates = []
-                        HSUpdates = []
-                        for FID,HID in zip_longest(FSIDs,HSIDs):
-                            if FID is not None:
-                                FSUpdates.append(self.Messages[Constants.Start][FID].values())
-                        print(f"Unit {FID + ":":<{Width//2}}Unit {HID}:")
-                        self.Messages.get
+                    rich.print(f"[underline]{Team} Start Of Turn Updates:[/underline]")
+                    TSMessages = []
+                    for ID in self.Messages[Constants.Start]:
+                        if str(Team)[0] in ID:
+                            TIndent:int = 4 
+                            TSMessages.append(ID)
+                            for TSMessage in self.Messages[Constants.Start][ID]:
+                                TSMessages.append(f"{' '*TIndent}{TSMessage['Announcement']}")
+                                if "Effect" in TSMessage:
+                                    TIndent += 4
+                                    TSMessages.append(f"{' '*TIndent}{TSMessage['Effect']}")
+                                    TIndent -= 4
+                    for TSMessage in TSMessages:
+                        print(f"{TSMessage}")
+                    self.Messages[Constants.Start].clear()
+                else:
+                    print("[italic]No Start of Turn Updates.[/italic]")
+            case Constants.End:
+                if len(self.Messages[Constants.End]) > 0:
+                    pass
 
-    def BadDisplay(self,State:Constants,Team:Constants):
-        if Team == Constants.Friendly:
-            for ID,Message in self.Messages[State].items():
-                rich.print(f"[underline]Unit {ID} Updates:[/underline]")
-                print(f"    {Message}")
-            self.Messages[State].clear()
-        else:
-            Width = get_terminal_size().columns()
-            FTitle = "[underline]Friendly Team Updates:[/underline]"
-            HTitle = "[underline]Hostile Team Updates:[/underline]" 
-            rich.print(f"{FTitle:<{Width//2}}{HTitle}")
-            ...
-
-    def Update(self,Event:Constants,PrincipalID:str,Attribute:int | bool | str,BValue:int | bool | str,AValue:int | bool | str,OtherID:list[str] = None):
+    def Update(self,Event:Constants,PrincipalID:str,Attribute:Any,BValue:Any,AValue:Any,OtherID:list[str] = None):
         match Event:
             case Constants.Attacking:
-                AttackAnnouncement:str = f"Unit {PrincipalID} was Attacked by Unit {OtherID[0]}!"
+                AttackAnnouncement:str = f"Unit {PrincipalID} attacked Unit {OtherID[0]}:"
                 AttackEffect:str = f"Health: {BValue} -> {AValue}"
-                self.Messages[Constants.End][PrincipalID][Constants.Attacking] = {"Announcement":AttackAnnouncement,"Effect":AttackEffect}
+                self.Messages[Constants.End][PrincipalID].append({"Announcement":AttackAnnouncement,"Effect":AttackEffect,"Event":Event})
             case Constants.UnitDeath:
-                DeathAnnouncement:str = f"Unit {PrincipalID} was Killed by Unit {OtherID[0]}!"
-                self.Messages[Constants.End][PrincipalID][Constants.UnitDeath] = {"Announcement":DeathAnnouncement}
+                DeathAnnouncement:str = f"Unit {PrincipalID} has killed Unit {OtherID[0]}!"
+                self.Messages[Constants.End][PrincipalID] = [{"Announcement":DeathAnnouncement,"Event":Event}]
             case Constants.Infliction:
-                inflictionAnnouncement:str = f"Unit {PrincipalID} was Inflicted with {Attribute} by Unit {OtherID[0]}!"
-                self.Messages[Constants.End][PrincipalID][Constants.Infliction] = {"Announcement":inflictionAnnouncement}
+                inflictionAnnouncement:str = f"Unit {PrincipalID} has inflicted Unit {OtherID[0]}! with {Attribute}"
+                self.Messages[Constants.End][PrincipalID].append({"Announcement":inflictionAnnouncement,"Event":Event})
             case Constants.Trigger:
-                TriggerAnnouncement:str = f"Unit {PrincipalID} was affected by {Attribute}!"
-                self.Messages[Constants.Start][PrincipalID][Constants.Trigger] = {"Announcement":TriggerAnnouncement}
+                TriggerAnnouncement:str = f"Unit {PrincipalID} was affected by {Attribute}, {Attribute.Turns} turns left"
+                self.Messages[Constants.Start][PrincipalID].append({"Announcement":TriggerAnnouncement,"Event":Event})
             case Constants.Clearing:
-                ClearingAnnouncement:str = f"Unit {PrincipalID} has been Cleared of {Attribute}!"
-                self.Messages[Constants.Start][PrincipalID][Constants.Clearing] = {"Announcement":ClearingAnnouncement}
+                ClearingAnnouncement:str = f"Unit {PrincipalID} has been cleared of {Attribute}!"
+                self.Messages[Constants.Start][PrincipalID].append({"Announcement":ClearingAnnouncement,"Event":Event})
             case Constants.Healing:
-                HealingAnnouncement:str = f"Unit {PrincipalID} was Healed by Unit {OtherID[0]}!"
+                HealingAnnouncement:str = f"Unit {PrincipalID} healed Unit {OtherID[0]}:"
                 HealingEffect:str = f"Health: {BValue} -> {AValue}"
-                self.Messages[Constants.End][PrincipalID][Constants.Healing] = {"Announcement":HealingAnnouncement,"Effect":HealingEffect}
+                self.Messages[Constants.End][PrincipalID].append({"Announcement":HealingAnnouncement,"Effect":HealingEffect,"Event":Event})
             case Constants.Consumption:
-                ConsumptionAnnouncement:str = f"Unit {PrincipalID} has Consumed Upgrade {OtherID[0]}!"
+                ConsumptionAnnouncement:str = f"Unit {PrincipalID} has consumed Upgrade {OtherID[0]}!:"
                 ConsumptionEffect:str = f"Unit {PrincipalID} {Attribute}: {BValue} -> {AValue}"
-                self.Messages[Constants.Shopping][PrincipalID][Constants.Consumption] = {"Announcement":ConsumptionAnnouncement,"Effect":ConsumptionEffect}
+                self.Messages[Constants.Shopping][PrincipalID].append({"Announcement":ConsumptionAnnouncement,"Effect":ConsumptionEffect,"Event":Event})
             case Constants.Blocked:
                 BlockAnnouncement:str = f"Unit {PrincipalID} blocked unit {OtherID[0]}'s attack on unit {OtherID[1]}!"
                 BlockEffect:str = f"Unit {PrincipalID}'s Health: {BValue} -> {AValue}"
-                self.Messages[Constants.End][PrincipalID][Constants.Blocked] = {"Announcement":BlockAnnouncement,"Effect":BlockEffect}
+                self.Messages[Constants.End][PrincipalID].append({"Announcement":BlockAnnouncement,"Effect":BlockEffect,"Event":Event})
+            case Constants.Shielding:
+                ShieldAnnouncement:str = f"Unit {PrincipalID} is shielding unit {OtherID[0]}!"
+                self.Messages[Constants.Start][PrincipalID].append({"Announcement":ShieldAnnouncement,"Event":Event})   
             case _:
                 raise ValueError("Unrecognised Event Type!")
 
